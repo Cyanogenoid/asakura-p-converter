4f3e6a96446fa00103bb68b1761e56b162cfa5f3
Yan Zhang <cyanogenoid@cyanogenoid.com>
2013-12-15 10:41:43 +0000

    Fix Asakura_Map tuple indentation

--- converter.py
+++ converter.py
@@ -3,11 +3,11 @@ import struct
 from collections import namedtuple
 
 Asakura_Map = namedtuple('Map',
-              ['tileset_name', 'background_name', 'background_music_name',
-               'tiles', 'enemies', 'items', 'moving_tiles', 'doors',
-               'bubble_key', 'key', 'chest', 'enemy_set', 'time',
-               'jump_reduction', 'inertia']
-          )
+                  ['tileset_name', 'background_name', 'background_music_name',
+                   'tiles', 'enemies', 'items', 'moving_tiles', 'doors',
+                   'bubble_key', 'key', 'chest', 'enemy_set', 'time',
+                   'jump_reduction', 'inertia']
+              )
 
 def read_ints(fd, byte_count):
     return struct.unpack('i' * byte_count, fd.read(4 * byte_count))
