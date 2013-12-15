071912944496f03a4f93ad0be21a0a21c7e77d67
Yan Zhang <cyanogenoid@cyanogenoid.com>
2013-12-15 10:00:43 +0000

    Add methods to aid reading ints and strings from a file

--- converter.py
+++ converter.py
@@ -1,4 +1,5 @@
 import sys
+import struct
 from collections import namedtuple
 
 Asakura_Map = namedtuple('Map',
@@ -7,3 +8,12 @@ Asakura_Map = namedtuple('Map',
                'bubble_key', 'key', 'chest', 'enemy_set', 'time',
                'jump_reduction', 'inertia']
           )
+
+def read_ints(fd, byte_count):
+    return struct.unpack('i' * byte_count, fd.read(4 * byte_count))
+
+def read_string(fd, length):
+    return fd.read(length)
+
+def skip_bytes(fd, byte_count):
+    fd.seek(byte_count, 1)
